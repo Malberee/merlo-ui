5f46b10f25068df83cfc376ca4d3f701f1478a7f
Malberee <82259650+Malberee@users.noreply.github.com>
2025-11-04 22:56:18 +0200

    fix: twMerge config

--- src/core/theme/utils/tw-merge-config.ts
+++ src/core/theme/utils/tw-merge-config.ts
@@ -4,13 +4,13 @@ export const COMMON_UNITS = ['small', 'medium', 'large']
 
 export const twMergeConfig: Partial<Config<string, string>> = {
   theme: {
-    opacity: ['disabled'],
     spacing: ['divider'],
-    borderWidth: COMMON_UNITS,
-    borderRadius: COMMON_UNITS,
+    radius: COMMON_UNITS,
   },
   classGroups: {
     'shadow': [{ shadow: COMMON_UNITS }],
+    'opacity': [{ opacity: ['disabled'] }],
     'font-size': [{ text: ['tiny', ...COMMON_UNITS] }],
+    'border-w': [{ border: COMMON_UNITS }],
   },
 }
